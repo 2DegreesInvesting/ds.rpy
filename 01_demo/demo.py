c673ae1400f127041ed2cb15d8437ddf30625f19
Mauro Lepore <maurolepore@gmail.com>
2022-07-26 06:52:42 -0700

    Update demo

--- 01_demo/demo.py
+++ 01_demo/demo.py
@@ -1,12 +1,13 @@
+# Included in Anaconda
 import nltk
 import spacy
-# conda install -c conda-forge spacy-model-en_core_web_sm
 
 sent = "Please book my flight to California."
 
 tokenized_sent_nltk = nltk.sent_tokenize(sent)
 [nltk.pos_tag(nltk.word_tokenize(word)) for word in tokenized_sent_nltk]
 
+# conda install -c conda-forge spacy-model-en_core_web_sm
 nlp = spacy.load("en_core_web_sm")
 spacy_doc = nlp(sent)
 
